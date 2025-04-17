9c484445f30f64074fee1b4c1c10d9972f747fe5
Franco Enrici <francoenrici@MacBook-Air-de-Franco.local>
2025-04-18 00:08:40 +0200

    back listo

--- app.js
+++ app.js
@@ -4,40 +4,40 @@ const books = require('./data/books.json');
 const app = express();
 const PORT = 3000;
 
-// 1. Route to get all books
+// 1. ruta para obtener todos los libros
 app.get('/all', (req, res) => {
   res.json(books);
 });
 
-// 2. Route to get the first book
+// 2. ruta para obtener el primer libro
 app.get('/first', (req, res) => {
   res.json(books[0]);
 });
 
-// 3. Route to get the last book
+// 3. ruta para obtener el último libro
 app.get('/last', (req, res) => {
   res.json(books[books.length - 1]);
 });
 
-// 4. Route to get the middle book (number 50 in the array)
+// 4. ruta para obtener el libro del medio (número 50 en el array)
 app.get('/middle', (req, res) => {
   const shakuntalaBook = books.find(book => book.title === "The recognition of Shakuntala");
   res.json(shakuntalaBook);
 });
 
-// 5. Route to get ONLY THE TITLE of Dante Alighieri's book
+// 5. ruta para obtener el titulo del libro de dante alighieri
 app.get('/author/dante-alighieri', (req, res) => {
   const danteBook = books.find(book => book.author === 'Dante Alighieri');
   res.json(danteBook.title);
 });
 
-// 6. Route to get ONLY THE COUNTRY of Charles Dickens' book
+// 6. ruta para obtener el pais del libro de charles dickens
 app.get('/country/charles-dickens', (req, res) => {
   const dickensBook = books.find(book => book.author === 'Charles Dickens');
   res.json(dickensBook.country);
 });
 
-// 7. Route to get PAGES AND YEAR of Miguel de Cervantes' book
+// 7. ruta para obtener paginas y ano del libro de miguel de cervantes
 app.get('/year&pages/cervantes', (req, res) => {
   const cervantesBook = books.find(book => book.author === 'Miguel de Cervantes');
   res.json({ 
@@ -46,20 +46,21 @@ app.get('/year&pages/cervantes', (req, res) => {
   });
 });
 
-// 8. Route to get THE NUMBER OF BOOKS from Spain
+// 8. ruta para obtener el nro de libros de españa
 app.get('/country/count/spain', (req, res) => {
   const spanishBooks = books.filter(book => book.country === 'Spain');
   res.json(spanishBooks.length);
 });
 
-// 9. Route to get TRUE/FALSE if there is at least one book from Germany
+// 9. ruta para obtener verdadero/falso si hay al menos un libro de alemania
 app.get('/country/at-least/germany', (req, res) => {
   const hasGermanBook = books.some(book => book.country === 'Germany');
   res.json(hasGermanBook);
 });
 
-// 10. Route to get TRUE/FALSE if all books have more than 200 pages
+// 10. ruta para obtener verdadero/falso  si todos los libros tienen más de 200 páginas
 app.get('/pages/all-greater/200', (req, res) => {
   const allBooksHaveMoreThan200Pages = books.every(book => book.pages > 200);
   res.json(allBooksHaveMoreThan200Pages);
-}); 
+});
+
